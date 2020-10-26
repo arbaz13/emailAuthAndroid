acb1b92ea7db68e483526eb4ad6144ded85308e0
maniya127 <51273773+maniya127@users.noreply.github.com>
2020-10-26 06:51:13 +0530

    Create MapsActivity.java
    Uses GPSTracker class to show current location on Google Maps Activity

---
+++ app/src/main/java/com/example/tuk_tuk/MapsActivity.java
@@ -0,0 +1,54 @@
+package com.example.tuk_tuk;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    GPSTracker gps;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        
+        //call GPSTracker class
+        gps = new GPSTracker(MapsActivity.this);
+
+        // check if GPS enabled
+        if(gps.canGetLocation()){
+
+            double latitude = gps.getLatitude();
+            double longitude = gps.getLongitude();
+
+            // Add a marker in Sydney and move the camera
+            LatLng sydney = new LatLng(latitude,longitude);
+            mMap.addMarker(new MarkerOptions().position(sydney).title("Your Location"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        }else{
+            // can't get location
+            // GPS or Network is not enabled
+            // Ask user to enable GPS/network in settings
+            gps.showSettingsAlert();
+        }
+
+    }
+}
