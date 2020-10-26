2e45d13420ff83518e616811e4d5644b1b4a6b85
maniya127 <51273773+maniya127@users.noreply.github.com>
2020-10-26 07:43:16 +0530

    Update MapsActivity.java
    Select Destination added!

--- app/src/main/java/com/example/tuk_tuk/MapsActivity.java
+++ app/src/main/java/com/example/tuk_tuk/MapsActivity.java
@@ -3,18 +3,22 @@ package com.example.tuk_tuk;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    Marker marker;
     GPSTracker gps;
+    int a=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +29,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
-    
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        
-        //call GPSTracker class
         gps = new GPSTracker(MapsActivity.this);
 
         // check if GPS enabled
@@ -39,16 +41,54 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             double latitude = gps.getLatitude();
             double longitude = gps.getLongitude();
 
-            // Add a marker in Sydney and move the camera
-            LatLng current = new LatLng(latitude,longitude);
-            mMap.addMarker(new MarkerOptions().position(current).title("Your Location"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(current));
+            LatLng locat = new LatLng(latitude,longitude);
+            mMap.addMarker(new MarkerOptions().position(locat).title("Your Location"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(locat));
+            mMap.getMaxZoomLevel();
         }else{
             // can't get location
             // GPS or Network is not enabled
             // Ask user to enable GPS/network in settings
             gps.showSettingsAlert();
         }
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                a=a+1;
+                if (marker != null) {
+                    marker.remove();
+                }
+                if(a<=1) {
+                    Toast.makeText(
+                            MapsActivity.this,
+                            "Lat : " + latLng.latitude + " , "
+                                    + "Long : " + latLng.longitude,
+                            Toast.LENGTH_LONG).show();
+
+                    LatLng locate = new LatLng(latLng.latitude, latLng.longitude);
+                    mMap.addMarker(new MarkerOptions().position(locate).title("Destination"));
+                }
+
+            }
+        });
+        mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+                                         @Override
+                                         public void onMarkerDragStart(Marker marker) {
+
+                                         }
+
+                                         @Override
+                                         public void onMarkerDrag(Marker marker) {
+
+                                         }
+
+                                         @Override
+                                         public void onMarkerDragEnd(Marker marker) {
+
+                                         }
+                                     }
+        );
 
     }
+
 }
