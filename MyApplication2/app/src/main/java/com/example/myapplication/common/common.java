d20bc2c4161534e8e7e43ca36be060b8c0279613
Arbaz khan <arboarbaz@gmail.com>
2020-11-01 14:27:04 +0530

    eA

---
+++ MyApplication2/app/src/main/java/com/example/myapplication/common/common.java
@@ -0,0 +1,8 @@
+package com.example.myapplication.common;
+
+import com.example.myapplication.Model.RiderModel;
+
+public class common {
+    public static final String RIDER_INFO_REFERENCE = "Riders" ;
+    public static RiderModel currentRider;
+}
